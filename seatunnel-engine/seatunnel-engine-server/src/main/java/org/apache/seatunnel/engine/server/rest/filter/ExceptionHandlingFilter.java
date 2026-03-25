cae1246b031b59a3dd4bfe6b014b01bbaaca4b64
xiaochen-zhou <598457447@qq.com>
2026-03-25 21:06:57 +0800

    [Improve][Zeta] Improve REST API error response with full stack trace

--- seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/filter/ExceptionHandlingFilter.java
+++ seatunnel-engine/seatunnel-engine-server/src/main/java/org/apache/seatunnel/engine/server/rest/filter/ExceptionHandlingFilter.java
@@ -22,6 +22,7 @@ import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.seatunnel.engine.server.rest.ErrResponse;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.seatunnel.shade.org.apache.commons.lang3.exception.ExceptionUtils;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -64,7 +65,7 @@ public class ExceptionHandlingFilter implements Filter {
         response.setContentType("application/json;charset=UTF-8");
 
         ErrResponse errorResponse = new ErrResponse();
-        errorResponse.setMessage(e.getMessage());
+        errorResponse.setMessage(ExceptionUtils.getStackTrace(e));
         errorResponse.setStatus("fail");
 
         String jsonResponse = objectMapper.writeValueAsString(errorResponse);
